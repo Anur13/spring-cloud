2da64dc5b4de4257417c5dce569eaba97772a585
Anur13 <akrupskyi13@gmail.com>
2025-02-17 19:36:30 +0100

    implement config server

--- project1/src/main/java/com/cloud/project/ServiceRestController.java
+++ project1/src/main/java/com/cloud/project/ServiceRestController.java
@@ -1,13 +1,24 @@
 package com.cloud.project;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ServiceRestController {
+    @Value("${test}")
+    String test;
+
+    @Value("${sad}")
+    String sad;
 
     @GetMapping("/helloWorld")
     public String helloWorld() {
+
+        System.out.println(test);
+        System.out.println(sad);
+
         return "Hello world from Service A!";
     }
 
